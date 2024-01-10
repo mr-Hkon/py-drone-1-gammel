376bf36d913897909f7b6a6ede204136f7c21a2e
mr-Hkon <hakonellingsen2006@gmail.com>
2024-01-10 10:56:35 +0100

    to kan kje endra på samma fil samtidig

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from djitellopy import Tello
 import cv2
 
-## asdjfhgsadfksf
-
-## hellot hsi sis tja tes t
 
 
-# i ama testing
 
-#ein komentar
-somethhing.idk()  if equals minecfraft {]
-sadfaskdfasfd
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
