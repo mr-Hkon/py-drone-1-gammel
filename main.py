764aa66b10f42384ee1a387909e54fcae56bb488
Haakon Storstein Haug <haakon.haug@bouvet.no>
2024-01-10 10:49:39 +0100

    test number 4

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello this is mrbeast")
+from djitellopy import Tello
+import cv2
+
+## asdjfhgsadfksf
+
+## hellot hsi sis tja tes t
+
+
+# i ama testing
+
+
+somethhing.idk()  if equals minecfraft {]
+sadfaskdfasfd
+}
