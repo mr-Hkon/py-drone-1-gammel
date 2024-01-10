07810c330e7d7e724f764255b193ad1bfff38d94
Haakon Storstein Haug <haakon.haug@bouvet.no>
2024-01-10 13:00:25 +0100

    First iteration of Code

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from djitellopy import Tello
 import cv2
 
-## asdjfhgsadfksf
-##asaskljashdfkljashdfkjhasdfkjhaskjdfasjkfd sfheeleelellfloasfdloasdfofoefalfo
-## hellot hsi sis tja tes t
+test = Tello()
 
+test.connect()
 
-# i ama testing
+test.streamon()
 
+print(test.get_battery())
 
-somethhing.idk()  if equals minecfraft {]
-sadfaskdfasfd
-}
+while True:
+
+    read_frame = test.get_frame_read()
+    myFrame = read_frame.frame
+    img = cv2.resize(myFrame, (320, 240))
+
+    cv2.imshow("MyResult", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
