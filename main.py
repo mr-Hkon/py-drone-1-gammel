1fc62517c317a3aa0261dc7733f710e4916feea2
mr-Hkon <hakonellingsen2006@gmail.com>
2024-01-17 13:22:41 +0100

    keyboard controlls med kamera feil

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from djitellopy import Tello
 import cv2
 import time
 import keyboard
+import os
 
 test = Tello()
 
@@ -14,46 +15,80 @@ test.let_right_velocity = 0
 test.up_down_velocity = 0
 test.speed = 0
 
+height = 0
 
 print(test.get_battery())
 
-
-async def screen():
-    while True:
-        read_frame = test.get_frame_read()
-        myFrame = read_frame.frame
-        img = cv2.resize(myFrame, (640, 480))
-
-        cv2.imshow("MyResult", img)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-
 while True:
-    screen()
+    read_frame = test.get_frame_read()
+    myFrame = read_frame.frame
+    img = cv2.resize(myFrame, (640, 480))
 
+    cv2.imshow("MyResult", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
     if keyboard.is_pressed('down arrow'):
-        test.land()
+        if height == 10:
+            test.land()
+            height = 0
+        elif height > 19:
+            test.move_down(50)
+            height -= 10
+        else:
+            print("height error 1")
+            test.land()
+        '''test.land()
         print("down pressed")
-        time.sleep(0.5)
+        time.sleep(0.5)'''
 
     if keyboard.is_pressed('up arrow'):
-        test.takeoff()
+        if height == 0:
+            test.takeoff()
+            height = 10
+            os.system('clear')
+            print("klar")
+        elif height > 9:
+            test.move_up(50)
+            height += 10
+        else:
+            print("height error 2")
+            test.land()
+        '''test.takeoff()
         print("up pressed")
         time.sleep(0.5)
-        print("trykk w")
+        print("trykk w")'''
 
     if keyboard.is_pressed('w'):
         print("trykka w")
         test.move_forward(50)
         print("gjekk framover")
-
     if keyboard.is_pressed('s'):
         print("trykka s")
         test.move_back(50)
         print("gjekk bakover")
+    if keyboard.is_pressed('a'):
+        print("trykka a")
+        test.move_left(50)
+        print("gjekk venstre")
+    if keyboard.is_pressed('d'):
+        print("trykka d")
+        test.move_right(50)
+        print("gjekk høyre")
 
+    '''rotation controlls'''
+    if keyboard.is_pressed('left arrow'):
+        test.rotate_counter_clockwise(45)
+    if keyboard.is_pressed('right arrow'):
+        test.rotate_clockwise(45)
 
-
+    ''' flips controlls'''
+    if keyboard.is_pressed('1'):
+        test.flip('l')  # left
+    if keyboard.is_pressed('3'):
+        test.flip('r')  # right
+    if keyboard.is_pressed('2'):
+        test.flip('f')  # front
+    if keyboard.is_pressed('x'):
+        test.flip('b')  # back
